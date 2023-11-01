7d25147cbb47b400440033e90f1ea47779541aff
Abdurahuman34 <143972158+Abdurahuman34@users.noreply.github.com>
2023-10-31 22:12:07 -0700

    Add files via upload

---
+++ scribt.rb
@@ -0,0 +1,21 @@
+# While loop
+counter = 1 
+while counter <= 20 
+    if counter % 2 == 0
+        "#{counter} is Even""
+    else
+        #{counter} is odd"
+    end
+
+  end  
+    
+  # if loop
+    for  num in 1..20 
+        if num % 2 == 0 
+            puts "#{num} is Even"
+        else
+            puts "#{num} is odd"
+        end
+
+     end   
+        
